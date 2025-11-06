15ecb4eadac2084b4f11e255fad4a58ef7cb9843
Gayathri Ethiraj <egayu3@gmail.com>
2025-11-05 22:04:13 -0600

    add rev str

---
+++ arrays_and_strings/reverse_string.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # Example - Odd length
+        # Input: ["h","e", "l", "l", "o"]
+        # Index: [ 0 , 1 ,  2 ,  3 ,  4 ]
+        # len_s = 5; mid_index = 1
+        
+        # Example - Even length
+        # Input: ["h","e", "l", "l", "o", "o"]
+        # Index: [ 0 , 1 ,  2 ,  3 ,  4 ,  5 ]
+        # len_s = 6; mid_index = 2
+        
+        last_index = len(s) - 1
+        mid_index = (len(s) // 2)
+            
+        for i in range(mid_index):
+            temp = s[i]
+            s[i] = s[last_index - i]
+            s[last_index - i] = temp
+            
+        return s
+
+s = Solution()
+print(s.reverseString(["h","e", "l", "l", "o"]))
+print(s.reverseString(["h","e", "l", "l", "o", "o"]))
---
+++ env.sh
@@ -0,0 +1,2 @@
+# To create a conda environment, use "conda-forge" channel
+conda create -y -n lc python=3.11 -c conda-forge --strict-channel-priority
